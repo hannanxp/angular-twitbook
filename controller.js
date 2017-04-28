df0c47b0c2e867f390eda47e0f8f2d308073e6a5
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-04-28 13:26:53 +0700

    controller dipisah

---
+++ controller.js
@@ -0,0 +1,17 @@
+angular.module('myApp').controller('TwitController', ['$scope', function ($scope) {
+
+    $scope.twits = [
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      "Ut enim ad minim veniam",
+      "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      "Consectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet"
+    ];
+
+    $scope.shareTwit = function () {
+      $scope.twits.unshift($scope.msg);
+      $scope.msg = "";
+    };
+
+  }]);
+
