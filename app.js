72a1c10ecfa5cc265d4d33617816c1351e8c18a9
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-04-28 13:08:16 +0700

    module & Controller

---
+++ app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', []).controller('TwitController', ['$scope', function ($scope) {
+
+    $scope.twits = [
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      "Ut enim ad minim veniam",
+      "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      "Consectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet"
+    ];
+
+  }]);
+
