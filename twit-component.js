a952492224113800713f1ee39dbb084a53501974
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-04-28 14:03:21 +0700

    XHR

--- twit-component.js
+++ twit-component.js
@@ -1,35 +1,11 @@
 angular.module('myApp').component('twitPage', {
   templateUrl: 'twit-template.html',
-  controller: function twitController() {
+  controller: function twitController($http) {
     var self = this;
 
-    self.twits = [
-      {
-        "id": 5,
-        "twit": "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-        "created": "2017-04-20 21:10:00"
-      },
-      {
-        "id": 4,
-        "twit": "Ut enim ad minim veniam",
-        "created": "2017-04-19 15:17:00"
-      },
-      {
-        "id": 2,
-        "twit": "Consectetur adipiscing elit",
-        "created": "2017-04-17 11:13:00"
-      },
-      {
-        "id": 2,
-        "twit": "Consectetur adipiscing elit",
-        "created": "2017-04-17 11:13:00"
-      },
-      {
-        "id": 1,
-        "twit": "Lorem ipsum dolor sit amet",
-        "created": "2017-04-16 12:35:00"
-      }
-    ];
+    $http.get('server/twits.json').then(function (response) {
+      self.twits = response.data;
+    });
 
     this.shareTwit = function () {
       var date = new Date();
