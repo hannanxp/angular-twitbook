98593f9d48984586b9dd8b07decbe6cd9a7e2ba9
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-05-03 10:36:14 +0700

    component

---
+++ friend-list-component.js
@@ -0,0 +1,30 @@
+angular.module('myApp').component('friendList', {
+  templateUrl: 'friend-list-template.html',
+  controller: function twitController($http) {
+    var self = this;
+    
+    self.friends = [
+      {
+        id: 2,
+        name: "Cras justo odio"
+      },
+      {
+        id: 3,
+        name: "Dapibus ac facilisis in"
+      },
+      {
+        id: 4,
+        name: "Morbi leo risus"
+      },
+      {
+        id: 5,
+        name: "Porta ac consectetur ac"
+      },
+      {
+        id: 6,
+        name: "Vestibulum at eros"
+      }
+    ];
+
+  }
+});
