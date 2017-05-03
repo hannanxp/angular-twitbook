bdd3c2fa96afd2b8eeb3086c5ca7d83c36bc9dd4
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-05-03 13:13:20 +0700

    Service

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 angular.module('myApp', []);
+
+//service style, probably the simplest one
+angular.module('myApp').service('toolboxServiceStyle', function () {
+  this.getTime = function () {
+    var d = new Date();
+    return d.toLocaleString();
+  };
+});
+
--- toolbox-controller.js
+++ toolbox-controller.js
@@ -1,6 +1,9 @@
-angular.module('myApp').controller('ToolboxCtrl', ['$scope', function ($scope) {
+angular.module('myApp').controller('ToolboxCtrl', ['$scope','toolboxServiceStyle',
+  function ($scope, toolboxServiceStyle) {
     var d = new Date();
-    
+
     $scope.currentTime = d.toLocaleString();
     
+    $scope.timeFromService = toolboxServiceStyle.getTime();
+
   }]);
