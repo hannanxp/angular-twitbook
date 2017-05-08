ad6b5ee3d03d551d6eff61871163da02ebfe9184
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-05-09 04:10:46 +0700

    oc lazyload on ui-router

--- app/app.routes.js
+++ app/app.routes.js
@@ -11,25 +11,46 @@ angular.module('myApp').config(['$locationProvider', '$stateProvider', '$urlRout
               name: 'home',
               url: '/',
               templateUrl: 'app/states/home/home.template.html',
-              controller: "homeController"
+              controller: "homeController", // This view will use homeController loaded below in the resolve
+              resolve: {
+                f1: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    // you can lazy load files for an existing module
+                    return $ocLazyLoad.load('app/states/home/home.controller.js');
+                  }]
+              }
             })
             .state({
               name: 'myprofile',
               url: '/myprofile',
               templateUrl: 'app/states/myprofile/myprofile.template.html',
-              controller: "myprofileController"
+              controller: "myprofileController", 
+              resolve: {
+                f1: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load('app/states/myprofile/myprofile.controller.js');
+                  }]
+              }
             })
             .state({
               name: 'friends',
               url: '/friends',
               templateUrl: 'app/states/friends/friends.template.html',
-              controller: "friendsController"
+              controller: "friendsController", 
+              resolve: {
+                f1: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load('app/states/friends/friends.controller.js');
+                  }]
+              }
             })
             .state({
               name: 'profile',
               url: '/profile/:userId',
               templateUrl: 'app/states/profile/profile.template.html',
-              controller: "profileController"
+              controller: "profileController", 
+              resolve: {
+                f1: ['$ocLazyLoad', function ($ocLazyLoad) {
+                    return $ocLazyLoad.load('app/states/profile/profile.controller.js');
+                  }]
+              }
             })
             ;
 
