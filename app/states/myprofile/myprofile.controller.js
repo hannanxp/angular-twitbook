00a86cc30097f6cf0ad7898f96fee9a75443b2b8
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-05-11 03:34:56 +0700

    test data post

--- app/states/myprofile/myprofile.controller.js
+++ app/states/myprofile/myprofile.controller.js
@@ -1,10 +1,12 @@
-angular.module('myApp').controller('myprofileController', ['$scope',
-  function ($scope) {
+angular.module('myApp').controller('myprofileController', ['$scope','$http',
+  function ($scope, $http) {
     
     $scope.user = {};
     
     $scope.saveUser = function() {
-      $scope.user = {};
+      $http.post('server/test.json',$scope.user, function(response){
+        console.log(response.data);
+      });
     };
     
     
