dbe3e82a1765e71e78e55f825983996de1ae3741
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-05-12 10:05:34 +0700

    vs

--- app/shared/toolbox/toolbox-controller.js
+++ app/shared/toolbox/toolbox-controller.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('ToolboxCtrl', ['$scope','toolboxServiceStyle','toolboxFactoryStyle','toolboxProviderStyle',
-  function ($scope, toolboxServiceStyle, toolboxFactoryStyle, toolboxProviderStyle) {
+angular.module('myApp').controller('ToolboxCtrl', ['$scope','toolboxServiceStyle','toolboxFactoryStyle','toolboxProviderStyle','$rootScope',
+  function ($scope, toolboxServiceStyle, toolboxFactoryStyle, toolboxProviderStyle, $rootScope) {
     var d = new Date();
 
     $scope.timeFromController = d.toLocaleString();
@@ -11,4 +11,16 @@ angular.module('myApp').controller('ToolboxCtrl', ['$scope','toolboxServiceStyle
     //$scope.greetingText = toolboxServiceStyle.sayHello();
     //$scope.greetingText = toolboxFactoryStyle.sayHello();
     $scope.greetingText = toolboxProviderStyle.sayHello();
+    
+    $scope.userName = "Budi";
+    $rootScope.userDepartement = "Developer";
+    
+  }]);
+
+
+angular.module('myApp').controller('OrganizationCtrl', ['$scope',
+  function ($scope) {
+    
+    $scope.userNames = ['Ali', 'Agus', 'Budi', 'Cucun', 'Heni', 'Yulianto'];
+    
   }]);
